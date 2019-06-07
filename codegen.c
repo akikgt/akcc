9fc535b92272c0b2801bf88cde8c5e774a646ac0
akikgt <komoria@oregonstate.edu>
2019-06-07 14:41:50 +0900

    simplify gen()

--- codegen.c
+++ codegen.c
@@ -1,5 +1,5 @@
 #include "xcc.h"
-static label_count = 0;
+static int label_count = 0;
 
 void gen_lval(Node *node) {
     if (node->ty != ND_IDENT) 
@@ -13,63 +13,62 @@ void gen_lval(Node *node) {
 }
 
 void gen(Node *node) {
-    if (node->ty == ND_RETURN) {
-        gen(node->lhs);
-        printf(" pop rax\n");
-        printf(" mov rsp, rbp\n");
-        printf(" pop rbp\n");
-        printf(" ret\n");
-        return;
-    }
 
-    if (node->ty == ND_IF) {
-        gen(node->cond); 
-        printf(" pop rax\n");
-        printf(" cmp rax, 0\n");
+    switch (node->ty) {
+        case ND_RETURN:
+            gen(node->lhs);
+            printf(" pop rax\n");
+            printf(" mov rsp, rbp\n");
+            printf(" pop rbp\n");
+            printf(" ret\n");
+            return;
 
-        if (!node->els) {
-            printf(" je .Lend%d\n", label_count);
+        case ND_IF:
+            gen(node->cond);
+            printf(" pop rax\n");
+            printf(" cmp rax, 0\n");
+
+            if (!node->els)
+            {
+                printf(" je .Lend%d\n", label_count);
+                gen(node->then);
+                printf(".Lend%d:\n", label_count);
+                label_count++;
+                return;
+            }
+
+            printf(" je .Lelse%d\n", label_count);
             gen(node->then);
+            printf(" jmp .Lend%d\n", label_count);
+            printf(".Lelse%d:\n", label_count);
+            gen(node->els);
             printf(".Lend%d:\n", label_count);
             label_count++;
             return;
-        }
 
-        printf(" je .Lelse%d\n", label_count);
-        gen(node->then);
-        printf(" jmp .Lend%d\n", label_count);
-        printf(".Lelse%d:\n", label_count);
-        gen(node->els);
-        printf(".Lend%d:\n", label_count);
-        label_count++;
+        case ND_NUM:
+            printf(" push %d\n", node->val);
+            return;
 
-        return;
+        case ND_IDENT:
+            gen_lval(node);
+            printf(" pop rax\n");
+            printf(" mov rax, [rax]\n");
+            printf(" push rax\n");
+            return;
+
+        case '=':
+            gen_lval(node->lhs);
+            gen(node->rhs);
+
+            printf(" pop rdi\n");
+            printf(" pop rax\n");
+            printf(" mov [rax], rdi\n");
+            printf(" push rdi\n");
+            return;
     }
 
-    if (node->ty == ND_NUM) {
-        printf(" push %d\n", node->val);
-        return;
-    }
-
-    if (node->ty == ND_IDENT) {
-        gen_lval(node);
-        printf(" pop rax\n");
-        printf(" mov rax, [rax]\n");
-        printf(" push rax\n");
-        return;
-    }
-
-    if (node->ty == '=') {
-        gen_lval(node->lhs);
-        gen(node->rhs);
-
-        printf(" pop rdi\n");
-        printf(" pop rax\n");
-        printf(" mov [rax], rdi\n");
-        printf(" push rdi\n");
-        return;
-    }
-
+    /// operators requiring two operands
     gen(node->lhs);
     gen(node->rhs);
 
